2a9e9d87c6de34947f0c725db08f19aff61abed3
aidenmart <126609298+aidenmart@users.noreply.github.com>
2023-09-26 09:09:10 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,48 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+
+public class MainApp {
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car car1 = new Car("car1", 1);
+      Car car2 = new Car("car2", 2);
+      Car car3 = new Car("car3", 3);
+      Car car4 = new Car("car4", 4);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
+      }
+
+      System.out.println();
+
+      try {
+         User user = userService.getUserByCar("Car2", 2);
+         System.out.println(user + " " + user.getCar());
+      } catch (NoResultException e) {
+         System.out.println("User not found");
+      }
+
+      context.close();
+   }
+}
